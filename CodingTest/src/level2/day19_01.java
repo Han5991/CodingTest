77c0c2410e5d3650eb6b0c540a4dac1d1daf7eb0
Han5991 <73056306+Han5991@users.noreply.github.com>
2020-12-01 19:59:41 +0900

    19일차 문제풀이

---
+++ CodingTest/src/level2/day19_01.java
@@ -0,0 +1,28 @@
+package level2;
+
+class Solution25 {
+	public String solution(int n) {
+		String[] a = { "4", "1", "2" };
+		String answer = "";
+
+		while (n > 0) {
+			int b = n % 3;
+			n /= 3;
+
+			if (b == 0) {
+				n--;
+			}
+			answer = a[b] + answer;
+		}
+		return answer;
+	}
+}
+
+public class day19_01 {
+
+	public static void main(String[] args) {
+		Solution25 solution25 = new Solution25();
+		System.out.println(solution25.solution(12));// 6,9 에러
+	}
+
+}
