5fea3aa0d7c827a6715df7b72efe59af9d425d0b
Han5991 <73056306+Han5991@users.noreply.github.com>
2020-12-14 23:11:41 +0900

    compare 메서드에 대해서 공부하였다

---
+++ CodingTest/src/level2/day30_01.java
@@ -0,0 +1,39 @@
+package level2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+class Solution37 {
+	public String solution(int[] numbers) {
+		String num[] = new String[numbers.length];
+		for (int i = 0; i < numbers.length; i++) {
+			num[i] = Integer.toString(numbers[i]);
+		}
+		Arrays.sort(num, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				return (o1 + o2).compareTo((o2 + o1));
+			}
+		});
+		if (num[num.length - 1].equals("0")) {
+			return "0";
+		}
+		String answer = "";
+		for (int i = num.length; i > 0; i--) {
+			answer += num[i - 1];
+		}
+
+		return answer;
+	}
+}
+
+public class day30_01 {
+
+	public static void main(String[] args) {
+		int[] a = { 3, 30, 34, 5, 9 };
+		int[] b = { 6, 10, 2 };
+		Solution37 solution37 = new Solution37();
+		System.out.println(solution37.solution(a));
+	}
+}
