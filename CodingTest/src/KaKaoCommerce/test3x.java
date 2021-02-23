fad18b6f9b2d1406ca69b1bd2ee82af1c560d53a
Han Sang-Wook <73056306+Han5991@users.noreply.github.com>
2021-02-23 22:40:57 +0900

    [위맥스테크 코딩테스트]

--- CodingTest/src/KaKaoCommerce/test3x.java
+++ CodingTest/src/KaKaoCommerce/test3x.java
@@ -1,13 +1,35 @@
 package KaKaoCommerce;
 
+import java.util.Arrays;
+
 public class test3x {
 
 	public static void main(String[] args) {
-		int sum = 0;
-		for (int i = 0; i < 101; i++) {
-				sum += i;
+		String answer3 = "";
+		int[] numbers = random(65);
+		int[] numbers2 = random(97);
+		for (int i = 0; i < 5; i++) {
+			answer3 += (char) numbers2[i];
+			answer3 += (char) numbers[i];
 		}
-		System.out.println(sum);
+		System.out.println(answer3);
+		char[] charArr = answer3.toCharArray();
+		Arrays.sort(charArr);
+		String answer4 = new String(charArr);
+		System.out.println(answer4);
 	}
 
+	public static int[] random(int a) {
+		int n[] = new int[5];
+		for (int insertCur = 0; insertCur < n.length; insertCur++) {
+			n[insertCur] = (int) (Math.random() * 26) + a;
+			for (int searchCur = 0; searchCur < insertCur; searchCur++) {
+				if (n[insertCur] == n[searchCur]) {
+					insertCur--;
+					break;
+				}
+			}
+		}
+		return n;
+	}
 }
